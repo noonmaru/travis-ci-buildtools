7055e80516b367a333126249441d8d64da85789b
Noonmaru <noonmaru10@gmail.com>
2020-09-04 04:51:24 +0900

    Init

---
+++ buildtools.sh
@@ -0,0 +1,16 @@
+echo "Downloading BuildTools.jar"
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar -O BuildTools.jar
+
+args=("$@")
+REPO_DIR="$HOME/.m2/repository/org/spigotmc/spigot/"
+
+for version in ${args[@]}; do
+	lines=$(find $REPO_DIR -name "$version-*" | wc -l)
+
+	if [ $lines -eq 0 ]; then
+		echo "Downloding spigot-$version"
+		java -jar ./BuildTools.jar --rev $version
+	else
+		echo "Skip downloading spigot-$version"
+	fi
+done
